27febc8164648f1459fc619d63fd470a5e87c584
raphson1 <raphsoneric@gmail.com>
2022-06-06 13:52:28 -0500

    repos creation and schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+CREATE TABLE departement(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    departement_id INT,
+    FOREIGN KEY (departement_id)
+    REFERENCES departement(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+);

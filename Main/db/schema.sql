df9690a125215b84ea48aae0048881e2b43e0fd6
raphson1 <raphsoneric@gmail.com>
2022-06-16 21:20:20 -0500

    Employe table update

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -15,6 +15,7 @@ CREATE TABLE role(
     departement_id INT,
     FOREIGN KEY (departement_id)
     REFERENCES departement(id)
+    ON DELETE SET NULL
 );
 
 CREATE TABLE employee(
@@ -22,9 +23,11 @@ CREATE TABLE employee(
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
-    manager_id INT,
+    manager_id INT NULL,
     FOREIGN KEY (role_id)
-    REFERENCES role(id),
+    REFERENCES role(id)
+    ON DELETE SET NULL,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
+    ON DELETE SET NULL
 );
--- Main/db/seeds.sql
+++ Main/db/seeds.sql
@@ -6,12 +6,20 @@ VALUES (4, "Engineering"),
 
 
 INSERT INTO role(id, title, Salary, departement_id) 
-VALUES(1, "lead Engineer", 150000, 4),
-      (6, "Software Engineer", 135000, 4),
-      (10, "Account Manager", 120000, 2),
-      (13, "Lawyer", 80000, 3),
-      (15, "Legal Team Lead", 90000, 3),
-      (18, "Security Lead", 70000, 9),
-      (23, "Accountant", 70000, 2),
-      (34, "Security", 65000, 9);
+VALUES (1, "lead Engineer", 150000, 4),
+       (6, "Software Engineer", 135000, 4),
+       (10, "Account Manager", 120000, 2),
+       (13, "Lawyer", 80000, 3),
+       (15, "Legal Team Lead", 90000, 3),
+       (18, "Security Lead", 70000, 9),
+       (23, "Accountant", 70000, 2),
+       (34, "Security", 65000, 9);
+
+
+INSERT INTO employee(id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Eric", "Ruboneka", 1),
+       (2, "Jack", "Joe", 13, 1),
+       (3, "Justin", "James", 6, 2),
+       (4, "Lebon", "Ngabire", 10, 2),
+       (5, "Abby", "Belicia", 15);
 
